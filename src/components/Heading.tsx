13d29999989b536c9bdf09727f1502da0ba39736
rami-dv <rami.dv99@gmail.com>
2024-05-14 08:06:02 -0300

    add arrowheading sponsors page

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,3 +1,5 @@
+import { PropsWithChildren } from "react";
+
 export default function Heading({
   bubble = false,
   color = null,
@@ -38,6 +40,46 @@ export default function Heading({
         {children}
       </div>
       <div className={`flex-1 border-t-2 ${borders[color ?? "red"]}`}></div>
+    </div>
+  );
+}
+
+export function ArrowHeading({
+  color = "red",
+  size = 0,
+  children,
+}: PropsWithChildren<{
+  color?: "green" | "orange" | "red" | "blue";
+  size?: 0 | 1 | 2 | 3;
+}>) {
+
+  const sizeClasses = {
+    0: "text-3xl sm:text-4xl",
+    1: "text-2xl sm:text-3xl",
+    2: "text-xl sm:text-2xl",
+    3: "text-lg sm:text-xl",
+  }
+
+  const borders = {
+    green: "border-f4g_green",
+    red: "border-f4g_red",
+    orange: "border-f4g_orange",
+    blue: "border-f4g_blue",
+  };
+
+  const text = {
+    green: "text-f4g_green",
+    red: "text-f4g_red",
+    orange: "text-f4g_orange",
+    blue: "text-f4g_blue",
+  };
+
+  return (
+    <div
+      className={`flex my-6 sm:my-8 items-center font-ubuntu font-semibold ${text[color]} ${sizeClasses[size]}`}
+    >
+      <div className="px-2 sm:px-4"><span className="text-f4g_orange">&gt;&gt;&gt;</span> {children}</div>
+      <div className={`flex-1 border-t-4 ${borders[color]}`}></div>
     </div>
   );
 }
