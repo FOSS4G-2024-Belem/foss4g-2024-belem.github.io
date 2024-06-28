c0c70afdec3504758d3abae66a7e3fb31a0adaef
rami-dv <rami.dv99@gmail.com>
2024-06-28 17:45:17 -0300

    Early Bird Period

--- src/components/TicketPricesTable.tsx
+++ src/components/TicketPricesTable.tsx
@@ -24,7 +24,8 @@ export default function TicketPricesTable() {
       "Gala Dinner Extra": "Gala Dinner Extra",
       "Excursion Belém do Pará": "Excursion Belém do Pará",
       "Early Bird": "Early Bird",
-      "Full Price": "Full Price"
+      "Full Price": "Full Price",
+      "Early Bird Dates": "Early Bird Period: Jun 10 2024 - Aug 23 2024"
     },
     "es": {
       "Ticket Type": "Tipo de Entrada",
@@ -37,7 +38,8 @@ export default function TicketPricesTable() {
       "Gala Dinner Extra": "Gala Dinner Extra",
       "Excursion Belém do Pará": "Excursion Belém do Pará",
       "Early Bird": "Early Bird",
-      "Full Price": "Full Price"
+      "Full Price": "Full Price",
+      "Early Bird Dates": "Periodo Early Bird: 10 Jun 2024 - 23 Ago 2024"
     },
     "pt": {
       "Ticket Type": "Tipo de Ingresso",
@@ -50,100 +52,104 @@ export default function TicketPricesTable() {
       "Gala Dinner Extra": "Gala Dinner Extra",
       "Excursion Belém do Pará": "Excursion Belém do Pará",
       "Early Bird": "Early Bird",
-      "Full Price": "Full Price"
+      "Full Price": "Full Price",
+      "Early Bird Dates": "Período Early Bird: 10 Jun 2024 - 23 Ago 2024"
     },
   }
 
   const i18n = i18nAll[language as "en"| "es" | "pt"];
 
-  return <table className="border-collapse border-spacing-x-2">
-    <thead>
-      <tr className="font-bold border-b border-b-black text-center">
-        <td className="px-2 py-1 text-left">{i18n["Ticket Type"]}</td>
-        <td className="px-2 py-1">
-          {i18n["Early Bird"]}
-          <div className="text-xs">(USD)</div>
+  return <>
+    <table className="border-collapse border-spacing-x-2">
+      <thead>
+        <tr className="font-bold border-b border-b-black text-center">
+          <td className="px-2 py-1 text-left">{i18n["Ticket Type"]}</td>
+          <td className="px-2 py-1">
+            {i18n["Early Bird"]}
+            <div className="text-xs">(USD)</div>
+            </td>
+          <td className="px-2 py-1">
+            {i18n["Full Price"]}
+            <div className="text-xs">(USD)</div>
           </td>
-        <td className="px-2 py-1">
-          {i18n["Full Price"]}
-          <div className="text-xs">(USD)</div>
-        </td>
-        <td className="px-2 py-1">
-          {i18n["Early Bird"]}
-          <div className="text-xs">(BRL)</div>
-        </td>
-        <td className="px-2 py-1">
-          {i18n["Full Price"]}
-          <div className="text-xs">(BRL)</div>
-        </td>
-      </tr>
-    </thead>
-    <tbody>
-      <tr className="border-b border-gray-300">
-        <td className="px-2 py-1">{i18n["Local (Brazil)"]}</td>
-        <td className="px-2 py-1"><USD v={149} /></td>
-        <td className="px-2 py-1"><USD v={249} /></td>
-        <td className="px-2 py-1"><BRL v={801} /></td>
-        <td className="px-2 py-1"><BRL v={1339} /></td>
-      </tr>
-      <tr className="border-b border-gray-300">
-        <td className="px-2 py-1">{i18n["Local (Argentina)"]}</td>
-        <td className="px-2 py-1"><USD v={100} /></td>
-        <td className="px-2 py-1"><USD v={230} /></td>
-        <td className="px-2 py-1"><BRL v={538} /></td>
-        <td className="px-2 py-1"><BRL v={1237} /></td>
-      </tr>
-      <tr className="border-b border-gray-300">
-        <td className="px-2 py-1">{i18n["Regional"]}</td>
-        <td className="px-2 py-1"><USD v={249} /></td>
-        <td className="px-2 py-1"><USD v={349} /></td>
-        <td className="px-2 py-1"><BRL v={1339} /></td>
-        <td className="px-2 py-1"><BRL v={1877} /></td>
-      </tr>
-      <tr className="border-b border-gray-300">
-        <td className="px-2 py-1">{i18n["International"]}</td>
-        <td className="px-2 py-1"><USD v={349} /></td>
-        <td className="px-2 py-1"><USD v={468} /></td>
-        <td className="px-2 py-1"><BRL v={1877} /></td>
-        <td className="px-2 py-1"><BRL v={2517} /></td>
-      </tr>
-      <tr className="border-b border-gray-300">
-        <td colSpan={5}>
-          <div className="flex items-center my-2">
-            <div className="flex-1 border-gray-400 border-t mx-4"></div>
-            <div className="font-bold text-gray-900">Optional Addons</div>
-            <div className="flex-1 border-gray-400 border-t mx-4"></div>
-          </div>
-        </td>
-      </tr>
-      <tr className="border-b border-gray-300">
-        <td className="px-2 py-1">{i18n["Workshop"]}</td>
-        <td className="px-2 py-1"><USD v={49} /></td>
-        <td className="px-2 py-1"><USD v={69} /></td>
-        <td className="px-2 py-1"><BRL v={263} /></td>
-        <td className="px-2 py-1"><BRL v={371} /></td>
-      </tr>
-      <tr className="border-b border-gray-300">
-        <td className="px-2 py-1">{i18n["B2B Ticket"]}</td>
-        <td className="px-2 py-1">-</td>
-        <td className="px-2 py-1"><USD v={20} /></td>
-        <td className="px-2 py-1">-</td>
-        <td className="px-2 py-1"><BRL v={100} /></td>
-      </tr>
-      <tr className="border-b border-gray-300">
-        <td className="px-2 py-1">{i18n["Gala Dinner Extra"]}</td>
-        <td className="px-2 py-1">-</td>
-        <td className="px-2 py-1"><USD v={50} /></td>
-        <td className="px-2 py-1">-</td>
-        <td className="px-2 py-1"><BRL v={269} /></td>
-      </tr>
-      <tr className="border-b border-gray-300">
-        <td className="px-2 py-1">{i18n["Excursion Belém do Pará"]}</td>
-        <td className="px-2 py-1"><USD v={85} /></td>
-        <td className="px-2 py-1"><USD v={90} /></td>
-        <td className="px-2 py-1"><BRL v={457} /></td>
-        <td className="px-2 py-1"><BRL v={484} /></td>
-      </tr>
-    </tbody>
-  </table>
+          <td className="px-2 py-1">
+            {i18n["Early Bird"]}
+            <div className="text-xs">(BRL)</div>
+          </td>
+          <td className="px-2 py-1">
+            {i18n["Full Price"]}
+            <div className="text-xs">(BRL)</div>
+          </td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr className="border-b border-gray-300">
+          <td className="px-2 py-1">{i18n["Local (Brazil)"]}</td>
+          <td className="px-2 py-1"><USD v={149} /></td>
+          <td className="px-2 py-1"><USD v={249} /></td>
+          <td className="px-2 py-1"><BRL v={801} /></td>
+          <td className="px-2 py-1"><BRL v={1339} /></td>
+        </tr>
+        <tr className="border-b border-gray-300">
+          <td className="px-2 py-1">{i18n["Local (Argentina)"]}</td>
+          <td className="px-2 py-1"><USD v={100} /></td>
+          <td className="px-2 py-1"><USD v={230} /></td>
+          <td className="px-2 py-1"><BRL v={538} /></td>
+          <td className="px-2 py-1"><BRL v={1237} /></td>
+        </tr>
+        <tr className="border-b border-gray-300">
+          <td className="px-2 py-1">{i18n["Regional"]}</td>
+          <td className="px-2 py-1"><USD v={249} /></td>
+          <td className="px-2 py-1"><USD v={349} /></td>
+          <td className="px-2 py-1"><BRL v={1339} /></td>
+          <td className="px-2 py-1"><BRL v={1877} /></td>
+        </tr>
+        <tr className="border-b border-gray-300">
+          <td className="px-2 py-1">{i18n["International"]}</td>
+          <td className="px-2 py-1"><USD v={349} /></td>
+          <td className="px-2 py-1"><USD v={468} /></td>
+          <td className="px-2 py-1"><BRL v={1877} /></td>
+          <td className="px-2 py-1"><BRL v={2517} /></td>
+        </tr>
+        <tr className="border-b border-gray-300">
+          <td colSpan={5}>
+            <div className="flex items-center my-2">
+              <div className="flex-1 border-gray-400 border-t mx-4"></div>
+              <div className="font-bold text-gray-900">Optional Addons</div>
+              <div className="flex-1 border-gray-400 border-t mx-4"></div>
+            </div>
+          </td>
+        </tr>
+        <tr className="border-b border-gray-300">
+          <td className="px-2 py-1">{i18n["Workshop"]}</td>
+          <td className="px-2 py-1"><USD v={49} /></td>
+          <td className="px-2 py-1"><USD v={69} /></td>
+          <td className="px-2 py-1"><BRL v={263} /></td>
+          <td className="px-2 py-1"><BRL v={371} /></td>
+        </tr>
+        <tr className="border-b border-gray-300">
+          <td className="px-2 py-1">{i18n["B2B Ticket"]}</td>
+          <td className="px-2 py-1">-</td>
+          <td className="px-2 py-1"><USD v={20} /></td>
+          <td className="px-2 py-1">-</td>
+          <td className="px-2 py-1"><BRL v={100} /></td>
+        </tr>
+        <tr className="border-b border-gray-300">
+          <td className="px-2 py-1">{i18n["Gala Dinner Extra"]}</td>
+          <td className="px-2 py-1">-</td>
+          <td className="px-2 py-1"><USD v={50} /></td>
+          <td className="px-2 py-1">-</td>
+          <td className="px-2 py-1"><BRL v={269} /></td>
+        </tr>
+        <tr className="border-b border-gray-300">
+          <td className="px-2 py-1">{i18n["Excursion Belém do Pará"]}</td>
+          <td className="px-2 py-1"><USD v={85} /></td>
+          <td className="px-2 py-1"><USD v={90} /></td>
+          <td className="px-2 py-1"><BRL v={457} /></td>
+          <td className="px-2 py-1"><BRL v={484} /></td>
+        </tr>
+      </tbody>
+    </table>
+    <div className="italic text-sm mt-2">* {i18n["Early Bird Dates"]}</div>
+  </>
 }
