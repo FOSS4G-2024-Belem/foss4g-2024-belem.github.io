55e2f0ce08d8d8c6ff5720966155cd4451821abe
rami-dv <rami.dv99@gmail.com>
2024-07-23 21:26:43 -0300

    make it all bigger

--- src/components/SponsorGrid.tsx
+++ src/components/SponsorGrid.tsx
@@ -9,6 +9,11 @@ export default function SponsorGrid({
   sponsorNames: Array<keyof typeof sponsors | null>;
 }) {
   const levelMap = {
+    [-1]: {
+      gap: "gap-3 sm:gap-8",
+      maxW: "max-w-40 6xl:max-w-60 max-h-16 6xl:max-h-20",
+      text: "p-3 6xl:p-5 text-2xl 6xl:text-4xl leading-5",
+    },
     0: {
       gap: "gap-4 6xl:gap-10",
       maxW: "",
@@ -16,22 +21,22 @@ export default function SponsorGrid({
     },
     1: {
       gap: "gap-3 sm:gap-8",
-      maxW: "max-w-40 6xl:max-w-60 max-h-16 6xl:max-h-20",
+      maxW: "max-w-96",
       text: "p-3 6xl:p-5 text-2xl 6xl:text-4xl leading-5",
     },
     2: {
-      gap: "gap-2 sm:gap-6",
-      maxW: "max-w-24 6xl:max-w-60 max-h-12 6xl:max-h-16",
+      gap: "gap-3 sm:gap-6",
+      maxW: "max-w-80",
       text: "p-2 6xl:p-4 6xl:text-xl leading-4",
     },
     3: {
-      gap: "gap-1 sm:gap-4",
-      maxW: "max-w-20 6xl:max-w-48",
+      gap: "gap-3 sm:gap-4",
+      maxW: "max-w-72",
       text: "p-1 6xl:p-3 text-sm 6xl:text-lg leading-4",
     },
     4: {
-      gap: "gap-1 sm:gap-2",
-      maxW: "max-w-20 6xl:max-w-36",
+      gap: "gap-3 sm:gap-2",
+      maxW: "max-w-64",
       text: "text-xs 6xl:text-base leading-4",
     },
   };
@@ -49,7 +54,7 @@ export default function SponsorGrid({
         >
           {sponsorName ? (
             <>
-              <div className="md:hidden">
+              <div className="md:hidden max-w">
                 <label htmlFor={`modal-${sponsorName}`}>
                   <Image
                     alt={`${sponsorName} logo`}
