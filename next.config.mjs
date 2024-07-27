67958ff71803e8a233257c356bd08ffa733d4fa9
rami-dv <rami.dv99@gmail.com>
2024-07-27 15:55:36 -0300

    use @rami-dv/mapajoara for conference map

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ import withMDX from "@next/mdx";
 const nextConfig = {
   output: "export",
   trailingSlash: true,
+  transpilePackages: ['@rami-dv/mapajoara'],
 
   images: {
     unoptimized: true,
@@ -20,6 +21,26 @@ const nextConfig = {
   basePath: undefined, //process.env.NODE_ENV == "production" ? "/foss4g-belem" : undefined,
   reactStrictMode: true,
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+  webpack: (config, context) => {
+
+    // exclude the sprite png's from next image loader
+    for (let rule of config.module.rules) {
+      if (rule["loader"] == "next-image-loader") {
+        rule["exclude"] = /\@rami\-dv\/mapajoara\/src\/map\/.*\.png$/i
+      }
+    }
+
+    // load map assets as an asset/resource
+    config.module.rules.push({
+      test: /\@rami\-dv\/mapajoara\/src\/map\/.*\.(pmtiles|pbf|png|json)$/i,
+      type: "asset/resource",
+      generator: {
+        filename: "static/chunks/[path][name][ext]",
+      },
+    });
+
+    return config;
+  },
 };
 
 export default withMDX()(nextConfig);
