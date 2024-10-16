b59cd558a50370616d9f86133a971d5153bb491b
Felipe Sodré <felipe.b4rros@gmail.com>
2024-10-16 18:08:26 -0300

    add sponsors to main homepage

---
+++ src/components/SponsorSection.tsx
@@ -0,0 +1,49 @@
+import Heading from "@/components/Heading";
+import SponsorGrid from "@/components/SponsorGrid";
+import Image from "next/image";
+import Link from "next/link";
+import IntlLink from "@/components/IntlLink";
+import ArrowHeading from "@/components/Heading"; // Assumindo que ArrowHeading seja reutilizável
+import Foss4g2022OpeningSession from "@/images/foss4g-2022/opening-session.jpg";
+
+export default function SponsorsSection() {
+  return (
+    <div className="space-y-12">
+     {/* Organizers */}
+      <div className="border border-f4g_orange max-w-[800px] mx-auto my-8">
+        <div className="bg-f4g_orange font-ubuntu text-white text-xl text-center py-1">
+          Organizers
+        </div>
+        <SponsorGrid
+          level={-1}
+          sponsorNames={["osgeo", "osgeobrasil", "geolibres", "ifpa"]}
+        />
+      </div>
+
+      {/* Sponsors por nível */}
+      <ArrowHeading size={0}>Diamond Sponsor</ArrowHeading>
+      <SponsorGrid level={0} sponsorNames={[null]} />
+
+      <ArrowHeading size={1}>Platinum Sponsors</ArrowHeading>
+      <SponsorGrid level={1} sponsorNames={[null, null]} />
+
+      <ArrowHeading size={2}>Gold Sponsors</ArrowHeading>
+      <SponsorGrid level={2} sponsorNames={["kan"]} />
+
+      <ArrowHeading size={2}>Silver Sponsors</ArrowHeading>
+      <SponsorGrid level={3} sponsorNames={["google", "sccon", "quarticle"]} />
+
+      <ArrowHeading size={2}>Bronze Sponsors</ArrowHeading>
+      <SponsorGrid level={4} sponsorNames={["sourcepole", "devseed", "arkedgespace", "merginmaps", "opengis_ch"]} />
+
+      <ArrowHeading size={2}>Supporter Sponsors</ArrowHeading>
+      <SponsorGrid level={5} sponsorNames={["geoone", "tomtom"]} />
+
+      <ArrowHeading size={2} color="orange">Community Partners</ArrowHeading>
+      <SponsorGrid level={-1} sponsorNames={["geochicas", "meninasdageo", "qgisbrasil", "qgisargentina"]} />
+
+      <ArrowHeading size={2} color="orange">Media Partners</ArrowHeading>
+      <SponsorGrid level={-1} sponsorNames={["zenodo", "ufra"]} />
+    </div>
+  );
+}
